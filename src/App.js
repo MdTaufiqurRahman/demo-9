484b91af5999f95b6e194551317093c6f6a032ac
MdTaufiqurRahman <taufiqur.anik.bd@gmail.com>
2022-10-28 10:27:11 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { Button, TextField, Typography } from "@mui/material";
+import { Stack } from "@mui/system";
 import { useEffect } from "react";
 import "./App.css";
 import { useAxiosRequest } from "./common/hooks/useAxiosRequest";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 function App() {
   const postsApi = useAxiosRequest([]);
@@ -8,6 +11,8 @@ function App() {
   const SalesData = () => {
     postsApi.apiAction({
       urlObjKey: "postsApi",
+      method: "GET",
+      isToast: true,
     });
   };
 
@@ -15,19 +20,49 @@ function App() {
     SalesData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  // console.log(postsApi?.resData);
 
   return (
     <>
-      <div className="App">
-        <p>Learn React</p>
-        {postsApi?.resData.map((item, index) => {
-          return (
-            <div key={index}>
-              <li>{item?.title}</li>
-            </div>
-          );
-        })}
+      <div className="container">
+        <div className="row">
+          <Typography variant="h5" gutterBottom>
+            Learn React
+          </Typography>
+         <div>
+         <TextField
+            id="outlined-basic"
+            label="Search Results"
+            variant="outlined"
+            required
+          />
+         </div>
+          {postsApi?.resData.map((item, index) => {
+            return (
+                <div className="col-md-4">
+                  <div className="card mt-2">
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        {item?.id}. {item?.title}
+                      </h5>
+                      <Typography variant="h6" gutterBottom></Typography>
+                      <Stack direction="row" spacing={2}>
+                        <Button variant="contained" color="success">
+                          Success
+                        </Button>
+                        <Button variant="contained">Add</Button>
+                        <Button variant="contained" color="warning">
+                          Warning
+                        </Button>
+                        <Button variant="contained" color="error">
+                          Remove
+                        </Button>
+                      </Stack>
+                    </div>
+                  </div>
+                </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
